61b9d3db0cba0a9c32b86d4dd09a59e7364b979f
codefine <you@277133779@qq.com>
2017-03-13 01:54:26 +0800

    登录页面初版

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,34 @@
+$.fn.extend({
+	'goRotate': function (direction){
+		var aChild = $(this).children();
+		var iLen = aChild.length;
+		var iHeight = $(this).height()/3;
+		var iNum = 1;
+		$(this).parent('.perspective').css('perspective',800);
+		$(this).parent('.z').css({
+			'transform': 'translateZ('+-iHeight+'px)',
+			'transform-style': 'preserve-3d'
+		});
+		$(this).css({
+			'transform-style': 'preserve-3d',
+			'transition': '2s'
+		});
+		aChild.each(function (i,elem){
+			$(elem).css({
+				'transform': ''+direction+'('+-i*360/iLen+'deg) translateZ('+iHeight+'px)',
+				'backface-visibility': 'hidden'
+			});
+		});
+		$(this).timer = setInterval(function (This){
+			This.css('transform',''+direction+'('+iNum*360/iLen+'deg)');
+			iNum++;
+		},4000,$(this));
+	}
+});
+
+$('.imgBox').goRotate('rotateY');
+$('.textBox').goRotate('rotateX');
+
+
+
+
