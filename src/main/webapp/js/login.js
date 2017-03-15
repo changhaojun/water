3d91269d27a4cd4d4aaae338702ed81c42d2bc56
MichaelLu <277133779@qq.com>
2017-03-15 17:29:58 +0800

    合并js

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,35 +1,60 @@
 $.fn.extend({
+	//左侧轮播
 	'goRotate': function (direction,offset,isRotate){
 		var aChild = $(this).children();
 		var iLen = aChild.length;
-		var iSideLen = direction==='X' ? $(this).height() : $(this).width();
-		var iDis = Math.floor(iSideLen/2*Math.tan(Math.PI*(90-360/iLen)/2/180))+offset;
-		$(this).parent('.perspective').css({
-			'perspective': 800,
-			'-webkit-perspective': 800
-		});
-		$(this).parent('.z').css({
-			'transform': 'translateZ('+-iDis+'px)',
-			'transform-style': 'preserve-3d',
-			'-webkit-transform': 'translateZ('+-iDis+'px)',
-			'-webkit-transform-style': 'preserve-3d'
-		});
-		$(this).css({
-			'transform-style': 'preserve-3d',
-			'-webkit-transform-style': 'preserve-3d'
-		});
-		aChild.each(function (i,elem){
-			$(elem).css({
-				'transform': 'rotate'+direction+'('+-i*360/iLen+'deg) translateZ('+iDis+'px)',
-				'backface-visibility': 'hidden',
-				'-webkit-transform': 'rotate'+direction+'('+-i*360/iLen+'deg) translateZ('+iDis+'px)',
-				'-webkit-backface-visibility': 'hidden'
+		var info = navigator.userAgent.toLowerCase();
+		if (info.indexOf("msie 9")>=0||info.indexOf("msie 8")>=0||info.indexOf("msie 7")>=0) {
+			//IE9以下浏览器：淡入淡出
+			var iIndex = 0;
+			turnHide();
+			function turnHide(){
+				aChild.css('opacity',0);
+				aChild.eq(iIndex).css('opacity',1).delay(2000).animate({
+					'opacity': 0
+				},2000);
+				iIndex++;
+				if (iIndex>iLen-1) {
+					iIndex = 0;
+				}
+				aChild.eq(iIndex).delay(2000).animate({
+					'opacity': 1
+				},2000,function (){
+					turnHide();
+				});
+			}
+		} else {
+			//标准浏览器：三棱柱旋转
+			var iSideLen = direction==='X' ? $(this).height() : $(this).width();
+			var iDis = Math.floor(iSideLen/2*Math.tan(Math.PI*(90-360/iLen)/2/180))+offset;
+			$(this).parent('.perspective').css({
+				'perspective': 800,
+				'-webkit-perspective': 800
 			});
-		});
-		if (isRotate) {
-			$(this).addClass('rotate'+direction+'');
+			$(this).parent('.z').css({
+				'transform': 'translateZ('+-iDis+'px)',
+				'transform-style': 'preserve-3d',
+				'-webkit-transform': 'translateZ('+-iDis+'px)',
+				'-webkit-transform-style': 'preserve-3d'
+			});
+			$(this).css({
+				'transform-style': 'preserve-3d',
+				'-webkit-transform-style': 'preserve-3d'
+			});
+			aChild.each(function (i,elem){
+				$(elem).css({
+					'transform': 'rotate'+direction+'('+-i*360/iLen+'deg) translateZ('+iDis+'px)',
+					'backface-visibility': 'hidden',
+					'-webkit-transform': 'rotate'+direction+'('+-i*360/iLen+'deg) translateZ('+iDis+'px)',
+					'-webkit-backface-visibility': 'hidden'
+				});
+			});
+			if (isRotate) {
+				$(this).addClass('rotate'+direction+'');
+			}
 		}
 	},
+	//水平垂直居中
 	'stayCenter': function (){
 		var This = $(this);
 		setPos($(this));
@@ -47,6 +72,7 @@ $.fn.extend({
 			});
 		}
 	},
+	//弹出层
 	'popUp': function (){
 		var msg;
 		$(this).click(function (){
@@ -81,6 +107,7 @@ $.fn.extend({
 			window.open(iUrl);
 		});
 	},
+	//文本框相应
 	'smartInput': function (){
 		$(this).focus(function (){
 			if ($(this).val()==='请输入用户名'||$(this).val()==='请输入密码'||$(this).val()==='请输入正确的邮箱地址') {
@@ -95,6 +122,7 @@ $.fn.extend({
 			}
 		});
 	},
+	//密码框相应
 	'passwordInput': function (){
 		$(this).focus(function (){
 			$(this).attr('type','password');
@@ -105,6 +133,7 @@ $.fn.extend({
 			}
 		});
 	},
+	//登录交互
 	'login': function (){
 		$(this).click(function (){
 			var iUsername = $('input').filter('[name=username]').val();
@@ -114,33 +143,38 @@ $.fn.extend({
 			if (iUsername==='请输入用户名'||iPassword==='请输入密码') {
 				return;
 			} else {
-				$.ajax({
-					type:"post",
-					dataType:"JSON",
-					url:"/authorize/authorize",
-					async:true,
-					data:{
-						'username': iUsername,
-						'password': iPassword,
-						'client_id': 'admin',
-						'client_secret': 'admin'
-					},
-					success: function (data){
-						console.log(data);
-						if(data.code==200){
-							console.info("123")
-							$.ajax({
-								url:'/lanyue-water/login',
-								type:'POST',
-								dataType:'JSON',
-								data:{'data':JSON.stringify(data)},
-								success:function(){
-									
-								}
-							})
-						} 
-					}
-				});
+				$.getJSON('http://chaxun.1616.net/s.php?type=ip&output=json&callback=?&_='+Math.random(), function(data){
+					var iAddress = data.Isp.split(' ')[0].toString();
+					$.ajax({
+						type:"post",
+						dataType:"JSON",
+						url:"http://192.168.1.104:80/authorize/authorize",
+						async:true,
+						data:{
+							'username': iUsername,
+							'password': iPassword,
+							'client_id': 'admin',
+							'client_secret': 'admin',
+							'code': '',
+							'address': iAddress
+						},
+						success: function (data){
+							console.log(data);
+							if(data.code==200){
+								console.info("123")
+								$.ajax({
+									url:'/lanyue-water/login',
+									type:'POST',
+									dataType:'JSON',
+									data:{'data':JSON.stringify(data)},
+									success:function(){
+										
+									}
+								})
+							}
+						}
+					});
+		      });
 			}
 		});	
 	}
