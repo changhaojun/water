28cd4be7a10f39aa7f5bbed35f18015af768d742
dongmo <113552669@qq.com>
2017-05-17 16:23:33 +0800

    完成计算数值的列表页面和新增编辑页面

---
+++ src/main/webapp/js/computed_data.js
@@ -0,0 +1,291 @@
+$.allData={
+	computedData:{
+		device_code:'',
+		device_name:'',
+		company_id:$('#companyId').val(),
+		company_code:$('#companyCode').val(),
+		device_kind:4,
+		status:1
+	},
+	dataConfigs:[
+
+	],
+	dataConfig:{
+		data_name:'',
+		calculate_formula:[],
+		status:1,
+		port_type:'CD'
+	},
+	ports:[],
+	selectPort:'',
+	access_token:'',
+	inputCheck:true,			//input框验证是否通过
+	device_id:$('#deviceId').val(),
+	dataIndex:999
+};
+
+var $extend=$.fn.extend({
+	openDataBox:function(titleMsg){
+		dataBox=layer.open({
+			type: 1,
+			title: titleMsg,
+			shadeClose: false,
+			shade: 0.8,
+			area: ['860px'],
+			content: $('.dataBox') //iframe的url
+		})
+	},
+	getThing:function(){		//获取触发条件的实体列表
+		var selectUl=$(this).next();
+		$.ajax({
+			type:"get",
+			url:globalurl+"/v1/things?access_token="+$.allData.access_token,
+			async:false,
+			data:{
+				like:'{"thing_name":"'+$(this).val()+'"}'
+			},
+			success:function(data){
+				selectUl.show();
+				selectUl.empty();
+				if(data.rows.length>0){
+					for(var i=0;i<data.rows.length;i++){
+						selectUl.append($('<li class="thingLi" thing_id="'+data.rows[i]._id+'">'+data.rows[i].thing_name+'</li>'));
+					}
+					selectUl.css("border-color","#E7E7E7");
+				}else{
+					selectUl.append($('<li class="thingLi" thing_id=0>未查询到该实体！</li>'));
+					selectUl.css("border-color","red");
+				}
+			}
+		});
+	},
+	changeThing:function(){	//选择实体后执行
+		var parent=$(this).parent();
+		var parentInput=parent.prev();
+		var classType=$(this).attr('class');
+		var thingId=$(this).attr('thing_id');
+		if(classType=="thingLi"){
+			parent.attr("thing_id",thingId);
+			parent.getTagList();
+			parentInput.val($(this).text());
+			parent.empty();
+			parent.hide();
+		}
+	},
+	getTagList:function(){		//选择实体后获取该实体下标签的列表
+		var tagSelect;
+		tagSelect=$(this).parent().next().find("select");
+		var thingId;
+		thingId=$(this).attr('thing_id');
+		$.ajax({
+			type:"get",
+			url:globalurl+"/v1/missionDataTags?access_token="+$.allData.access_token+"&thing_id="+thingId,
+			async:false,
+			data:{
+				filter:'{"port_type":"AI","port_type_1":"MO"}'
+			},success:function(data){
+				if(data.length>0){
+					$.allData.ports=data
+				}
+			}
+		});
+	},
+	selectTag:function(){
+		if($(this).val()!=0){
+			var selectData=$(this).find("option:selected").text();
+			var value=$.allData.selectPort;
+			var formula={
+				"data_name":selectData,
+				"class_name":"dataTag",
+				"data_tag":value
+				
+			};
+			$.allData.dataConfig.calculate_formula.push(formula);
+		}
+	},
+	addSign:function(){
+			var selectSign=$(this).text();
+			var formula={
+				"data_name":selectSign,
+				"class_name":"selectSign",
+				"dataTag":selectSign
+				
+			};
+			$.allData.dataConfig.calculate_formula.push(formula);
+		
+	},
+	editPort:function(index,event){
+		$.allData.dataIndex=index;
+		var $This = typeof event==="undefined" ? $(this) : $(event.currentTarget);
+		$.allData.dataConfig = $.allData.dataConfigs[index]
+		var titleMsg="修改计算公式"
+		$This.openDataBox(titleMsg)
+	},
+	deletePort:function(index,event){
+		var $This = typeof event==="undefined" ? $(this) : $(event.currentTarget);
+		$.allData.dataConfigs.splice(index,1)
+	},
+});
+
+$.extend({
+	init:function(){
+		var vmDevice = new Vue({
+			el:'#vue',
+			data:$.allData,
+			methods:$extend
+		});
+		getToken();
+		$.allData.access_token = accesstoken;
+		$.addPort();
+		$(document).delegate('*','click',function(ev){
+			if($(ev.target).attr('class')==='analogInput'){
+				$('.analogCursor').removeAttr("hidden")
+			}else{
+				$('.analogCursor').attr("hidden","hidden")
+			}
+			return false;
+		});
+		$(".thing").keyup(function(){
+			$(this).getThing();
+		});
+		$("ul").delegate("li","click",function(){
+			$(this).changeThing();
+		});
+		$('.conditionTag').change(function(){
+			$(this).selectTag();
+		});
+		$('.sign').click(function(){
+			$(this).addSign();
+		});
+		$('.saveBtn').click(function(){
+			$.saveDataConfig();
+		});
+		$('#main-submit').click(function(){
+			$.saveDevice();
+		});
+		$(document).keydown(function (event) {
+			if($('.analogCursor').attr("hidden")==undefined){ 
+				event.preventDefault();
+				if(event.keyCode==8){
+					if($('.analogCursor').prev().prop('tagName')=='DIV'){
+						var deleteIndex=$('.analogCursor').index()-1
+						$.allData.dataConfig.calculate_formula.splice(deleteIndex,1)
+					}
+				}else if(event.keyCode==37){
+					if($('.analogCursor').prev().prop('tagName')=='DIV'){
+						$('.analogCursor').insertBefore($('.analogCursor').prev())
+					}
+				}else if(event.keyCode==39){
+					if($('.analogCursor').next().prop('tagName')=='DIV'){
+						$('.analogCursor').insertAfter($('.analogCursor').next())
+					}
+				}
+			}
+		});
+		$.getDevice();
+	},
+	addPort:function(){
+		$('.addPort').click(function(){
+			var titleMsg="新增计算公式"
+			$.allData.dataConfig={
+				data_name:'',
+				calculate_formula:[],
+				status:1,
+				port_type:'CD'
+			};
+			$.allData.ports=[];
+			$.allData.dataIndex=999;
+			$(this).openDataBox(titleMsg)
+		})
+	},
+	saveDataConfig:function(){
+		if($.allData.dataIndex==999){
+			$.allData.dataConfigs.push($.allData.dataConfig);
+		}else{
+			$.allData.dataConfigs.splice($.allData.dataIndex,1,$.allData.dataConfig)
+		}
+		layer.close(dataBox);
+	},
+	saveDevice:function(){
+		var sendType,sendDevUrl;
+		if($.allData.device_id==''){
+			sendType='POST';
+			sendDevUrl=globalurl+"/v1/devices"
+		}else{
+			sendType='PUT';
+			delete $.allData.computedData._id;
+			sendDevUrl= globalurl+"/v1/devices/"+$.allData.device_id
+		}
+		$.ajax({
+			type:sendType,
+			dataType:'JSON',
+			url:sendDevUrl,
+			async: true,
+			crossDomain: true == !(document.all),
+			data: {
+				access_token: $.allData.access_token,
+				data: JSON.stringify($.allData.computedData)
+			},
+			success:function(data){
+				if(data._id){
+					$.allData.device_id=data._id
+				}
+				if(data.code==200){
+					$.ajax({
+						type: sendType,
+							dataType: "json",
+							url: globalurl+"/v1/devices/"+$.allData.device_id+"/dataConfigs",
+							async: true,
+							crossDomain: true == !(document.all),
+							data: {
+								access_token: $.allData.access_token,
+								data: JSON.stringify($.allData.dataConfigs)
+							},
+							success:function(data){
+								if(data.code==200){
+									layer.msg(data.success,{
+										icon:1,
+										end:function(){
+											self.location.href='/finfosoft-water/dataTag/computedEntry/'
+										}
+										})
+								}
+							}
+					});
+				}
+			}
+		})
+	},
+	getDevice:function(){
+		if($.allData.device_id!=''){
+			$.ajax({
+				type: "get",
+				dataType: "json",
+				url: globalurl+"/v1/devices/"+$.allData.device_id,
+				async: true,
+				crossDomain: true == !(document.all),
+				data: {
+					access_token:$.allData.access_token
+				},                                                                                                                                                                                                                                                                                
+				success: function(data) {
+					$.allData.computedData=data
+				}
+			});
+			$.ajax({
+				type: "get",
+				dataType: "json",
+				url: globalurl+"/v1/devices/"+$.allData.device_id+"/dataConfigs",
+				async: true,
+				crossDomain: true == !(document.all),
+				data: {
+					access_token: $.allData.access_token,
+					filter: JSON.stringify({device_id: $.allData.device_id})
+				},
+				success: function(data) {
+					$.allData.dataConfigs=data.rows
+				}
+			});
+		}
+	}
+});
+$.init();
