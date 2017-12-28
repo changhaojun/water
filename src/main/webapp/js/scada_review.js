09679c33fc4e3937eb0abf1f086fb89cc3c3eba3
dongmo <113552669@qq.com>
2017-12-28 10:31:01 +0800

    update

--- src/main/webapp/js/scada_review.js
+++ src/main/webapp/js/scada_review.js
@@ -2,8 +2,8 @@ var allData = {
 	thingId: $('#thingId').val(),
 	companyId: $('#companyId').val(),
 	accessToken: "",
-//	lanyueSrc: 'http://121.42.253.149:18822/scada',
-	lanyueSrc: 'http://localhost:80/scada',
+	lanyueSrc: 'http://121.42.253.149:18822/scada',
+//	lanyueSrc: 'http://localhost:80/scada',
 	ajaxComplated: {},
 	mqtt: { //MQTT订阅数据
 		host: mqttHostIP,
